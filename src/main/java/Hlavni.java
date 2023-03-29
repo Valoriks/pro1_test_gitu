4fc59d5ecb138d10a03da1874661630501914e6b
Valoriks <kuka5080@gmail.com>
2023-03-29 10:57:05 +0200

    Okomentování tříd

--- src/main/java/Hlavni.java
+++ src/main/java/Hlavni.java
@@ -1,3 +1,7 @@
+/**
+ * Třída Hlavní je hlavní třída projektu
+ */
+
 public class Hlavni {
     public static void main(String[] args) {
         System.out.println("Ahoj");
--- src/main/java/Trida.java
+++ src/main/java/Trida.java
@@ -1,3 +1,7 @@
+/**
+ * lmao
+ */
+
 public class Trida {
     public static void main(String[] args) {
         System.out.println("Jsem třída Trida.");
